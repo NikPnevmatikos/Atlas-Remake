c26ab7ae49a1f4d086c8f139cbd9c7e170eb3613
NikPnevmatikos <sdi1900157@di.uoa.gr>
2023-01-18 00:24:31 +0200

    back end first commit

---
+++ atlas/api/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#Intership provider
+class Provider(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    # companyName = models.CharField(max_length=30, blank=True)
+    providerType = models.CharField(max_length=30, blank=True)
+    category = models.CharField(max_length=30, blank=True)
+    afm = models.CharField(max_length=20, null=True, blank=True)
+    #phone = PhoneNumberField(blank = True, null = True)
+    workers = models.IntegerField(null=True, blank=True, default=0)
+    country = models.CharField(max_length=30, blank=True)
+    street = models.CharField(max_length=30, blank=True)
+    postal = models.IntegerField(null=True, blank=True, default=0)
+    name = models.CharField(max_length=30, blank=True)
+    identification = models.CharField(max_length=30, blank=True)
+
+    _id = models.AutoField(primary_key=True, editable=False)  
+
+    def __str__(self):
+        return self.user.username
+    
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    university = models.CharField(max_length=30, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+class Internship(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    university = models.CharField(max_length=200, null=True, blank=True)
+    lenght = models.CharField(max_length=200, null=True, blank=True)
+    type = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=30, blank=True)
+    price = models.IntegerField(null=True, blank=True, default=0)
+    hidden = models.BooleanField(null=True, blank=True, default=False)
+    
+    _id = models.AutoField(primary_key=True, editable=False)   
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Apply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    internship = models.ForeignKey(Internship, on_delete=models.CASCADE, null=True) 
+    state = models.CharField(max_length=200, null=True, blank=True)
+    cv = models.FileField(upload_to='' , null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    rejectionText = models.TextField(null=True, blank=True)
+    
+    _id = models.AutoField(primary_key=True, editable=False)   
+    
+    def __str__(self):
+        return self.internship.name
+    
---
+++ atlas/api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   #users
+   path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+   path('users/register/student/', views.registerStudent),
+   path('users/register/provider/', views.registerProvider),
+   path('users/update/', views.updateUserProfile),
+   path('users/profile/', views.getUserProfile),
+   #internships
+   path('internships/', views.all_internship),
+   path('internships/delete/<str:pk>/', views.delete_internship),
+   path('internships/create/', views.create_internship),
+   path('internships/update/<str:pk>/', views.update_internship),
+   path('internships/<str:pk>/', views.internship),
+   #apply
+   path("apply/view/<str:pk>/", views.applies),
+   path("apply/update/<str:pk>/", views.update_apply),
+   path("apply/delete/<str:pk>/", views.delete_apply),
+   path("apply/<str:pk>/", views.create_apply)
+]
